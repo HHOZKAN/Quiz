7650d2378f8ad924d86fc2347068ea4e397864d1
HHOZKAN <ozkn.haci@gmail.com>
2023-10-17 08:39:46 +0200

    quiz

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Quel mot-clé est utilisé pour déclarer une variable en JavaScript?",
+        answer: "D. let",
+        options: [
+            "A. var",
+            "B. int",
+            "C. string",
+            "D. let"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Comment pouvez-vous ajouter un commentaire sur une seule ligne dans du code JavaScript?",
+        answer: "A. // Commentaire",
+        options: [
+           "A. // Commentaire",
+           "B. /* Commentaire */",
+           "C. -- Commentaire",
+            "D. ## Commentaire",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Quelle méthode est utilisée pour ajouter un élément à la fin d'un tableau en JavaScript?",
+        answer: "A. push()",
+        options: [
+            "A. push()",
+            "B. add()",
+            "C. append()",
+            "D. insert()",
+        ]
+    },
+    {
+        numb: 4,
+        question: "Quelle boucle est principalement utilisée pour itérer à travers les propriétés d'un objet en JavaScript?",
+        answer: "A. for-in",
+        options: [
+            "A. for-in",
+            "B. while",
+            "C. do-while",
+            "D. for-of",
+        ]
+    },
+    {
+        numb: 5,
+        question: "Comment peut-on inclure un fichier JavaScript externe dans une page HTML?",
+        answer: "A. script src='monfichier.js' script",
+        options: [
+            "A. script src='monfichier.js' script",
+            "B. javascript src='monfichier.js' javascript",
+            "C. link rel='js' href='monfichier.js'",
+            "D. include src='monfichier.js'",
+        ]
+    }
+]
